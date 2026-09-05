602f740bca7e7a01b4fa06fe144819520614e33b
Nilda Dalir <nila.dalirnia@gmail.com>
2026-09-05 16:09:29 +0330

    feat: update CharacterResponse model to add unlocked, friendship_level, and fix formatting

--- backend/schemas/character.py
+++ backend/schemas/character.py
@@ -2,6 +2,15 @@ from pydantic import BaseModel
 
 
 class CharacterResponse(BaseModel):
+
     name: str
+
     species: str | None
+
     franchise: str | None
+
+    unlocked: bool | None
+
+    friendship_level: int | None
+
+    role: str | None
